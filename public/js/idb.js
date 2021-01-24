35e6e12361ea3090d62731ee1d0d323ba871d1d0
penaone <penaone@gmail.com>
2021-01-23 18:43:27 -0700

    added manifest.json

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,29 +1,69 @@
-// create variable to hold db connection
+// declare db variable
 let db;
-// establish a connection to IndexedDB database called 'budgettracker' and set it to version 1
-const request = indexedDB.open('budgettracker', 1);
+const request = indexedDB.open("pwa_budget", 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
-request.onupgradeneeded = function(event) {
-  // save a reference to the database 
+// listener runs when version changes or first connected
+request.onupgradeneeded = function (event) {
   const db = event.target.result;
-  // create an object store (table) called `budget_item`, set it to have an auto incrementing primary key of sorts 
-  db.createObjectStore('budget_item', { autoIncrement: true });
+
+  // create an object store (table) called 'budget_item', set it to have auto increment
+  db.createObjectStore("budget_item", { autoIncrement: true });
 };
 
-// upon a successful 
-request.onsuccess = function(event) {
-  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+request.onsuccess = function (event) {
   db = event.target.result;
 
-  // check if app is online, if yes run uploadBudget() function to send all local db data to api
   if (navigator.onLine) {
-    // we haven't created this yet, but we will soon, so let's comment it out for now
-    // uploadPizza();
+    uploadExpenses();
   }
 };
 
-request.onerror = function(event) {
-  // log error here
+request.onerror = function (event) {
   console.log(event.target.errorCode);
 };
+
+// save data will run in index.js
+// opens transaction then accesses expenseObjectStore to save data on form submit
+function saveData(data) {
+  // open transaction
+  const transaction = db.transaction(["budget_item"], "readwrite");
+
+  // access object store
+  const expenseObjectStore = transaction.objectStore("budget_item");
+
+  expenseObjectStore.add(data);
+}
+
+function uploadExpenses() {
+  const transaction = db.transaction(["budget_item"], "readwrite");
+  const expenseObjectStore = transaction.objectStore("budget_item");
+  const getAll = expenseObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    // if any new data in object store; sync with db
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(["budget_item"], "readwrite");
+          const expenseObjectStore = transaction.objectStore(["budget_item"]);
+          expenseObjectStore.clear();
+          alert("All expenses have beensubmitted");
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+}
+
+window.addEventListener("online", uploadExpenses);
--- public/js/index.js
+++ public/js/index.js
@@ -135,7 +135,7 @@ function sendTransaction(isAdding) {
     }
   })
   .catch(err => {
-    // fetch failed, so save in indexed db
+      // fetch failed, so save in indexed db
     saveRecord(transaction);
 
     // clear form
